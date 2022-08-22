dcad22cd0e949bc7f0699419bbe34a5ed25560cb
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-22 14:11:39 +0300

    commit tree cafa12fa4b58eb5d752c473d61882d6904bcd72e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+puts badge_maker("Paul")
+
+def batch_badge_creator(name)
+
+  name.map do |speaker|
+    badge_maker(speaker)
+
+  end
+end
+
+puts batch_badge_creator(["Harry", "John", "Paul"])
+
+def assign_rooms(speaker)
+  num = 0
+  speaker.map do |person|
+    num += 1
+    "Hello, #{person}! You'll be assigned to room #{num}!"
+  end
+end
+
+puts assign_rooms(["Harry", "John", "Paul", "Andrew"])
+
+def printer(speakers)
+
+  printer_array = [batch_badge_creator(speakers),
+  assign_rooms(speakers)]
+
+  printer_array.each do |m|
+     m.each {|n| puts n}
+  end
+end
+
+puts printer(["Harry", "John", "Paul"])
+
